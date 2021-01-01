d2778e21395b119d343e9bf9643daf68476660c3
eonsang <2eonhworking@gmail.com>
2021-01-02 05:35:32 +0900

    #1 end

---
+++ nuber-eats-backend/src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,17 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+
+@Resolver(() => Restaurant)
+export class RestaurantsResolver {
+  @Query(() => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+
+  @Mutation(() => Boolean)
+  createRestaurant(@Args() CreateRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(CreateRestaurantDto);
+    return true;
+  }
+}
